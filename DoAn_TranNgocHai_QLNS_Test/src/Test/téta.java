63554408a95078457468c7a93278ce76e4667582
asus <asus@LAPTOP-T2QAJEDG>
2020-12-04 09:42:29 +0700

    hêlo

---
+++ DoAn_TranNgocHai_QLNS_Test/src/Test/téta.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class téta {
+	public static String a;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("abcdsđs" + a);
+	}
+
+}
